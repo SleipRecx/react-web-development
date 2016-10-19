c90ba0037ba2d68d3142d3e3c0e081c59a6b60bb
Markus Andresen <markua@stud.ntnu.no>
2016-10-19 11:24:50 +0200

    app now single page

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,5 @@
 import React, { Component} from 'react';
+import {Link} from 'react-router';
 
 import '../../public/style.css';
 import navigationOptions from '../data/navigationOptions';
@@ -10,7 +11,7 @@ export default class Navigation extends Component {
                 <ul className="list-group">
                     {navigationOptions.map(option =>
                     <li key={option.id} className="list-group-item">
-                        <a href={option.route} className="btn-link">{option.title}</a>
+                        <Link to={option.route}> {option.title}</Link>
                     </li>
                     )}
                 </ul>
