54b922a543c7af97b746d693040ae2705e676cfa
Markus Andresen <markua@stud.ntnu.no>
2016-10-22 15:38:39 +0200

    data generation using faker implemented

--- src/components/ResultTable.js
+++ src/components/ResultTable.js
@@ -1,10 +1,50 @@
 import React, { Component} from 'react';
 import Rater from 'react-rater'
 import '../../public/styles/style.css';
-import books from '../data/books';
 import label_converter from '../data/label_converter';
 import Search from './Search';
 import Button from './Button';
+var faker = require('faker');
+var books = generate_data();
+
+
+function toTitleCase(str){
+   return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+
+function generate_data(){
+  var array = [];
+  var book_states = ['New','Readable',"Normal Use","As New"]
+  for(var i=0;i<24; i++){
+     var object = {}
+     object.id=i;
+     object.image= faker.image.avatar();
+     object.user = faker.name.firstName();
+     object.price =Math.round(faker.commerce.price()) + " kr";
+     object.added = faker.date.past().toDateString();
+     object.userRating = Math.floor((Math.random() * 6));
+     object.state = book_states[Math.floor((Math.random() * 4))];
+     var run = true;
+     while(run){
+        run = false;
+        var word = faker.random.words();
+
+        if(word.length > 24){
+           run = true;
+        }
+        if(word.length < 14){
+           run = true;
+        }
+     }
+     object.title = toTitleCase(word);
+     array.push(object);
+  }
+  return array;
+
+}
+
+
+
 
 class Content extends Component{
 
@@ -22,9 +62,13 @@ class Content extends Component{
         this.setState({searchString:e.target.value});
     }
 
+
+
     setSearch(e){
         this.setState({searchParameter:e.target.getAttribute('data-value')});
     }
+
+
 
     render() {
         var searchString = this.state.searchString.trim().toLowerCase();
@@ -39,10 +83,11 @@ class Content extends Component{
             });
         }
 
+
+
         return (
             <div className="bookTable">
                 <div className="input-group">
-                    
                 <Button onChange={this.setSearch}/>
                 <Search value={this.state.searchString} onChange={this.handleChange} />
                 </div>
@@ -59,13 +104,14 @@ class Content extends Component{
                         <th>Added</th>
                     </tr>
                     </thead>
+
                     <tbody>
                         {search_books.map(function(l){ return (
                         <tr key={"book"+ l.id}>
                             <td>{l.title}</td>
                             <td><span className={"label label-" + label_converter(l.state)} >{l.state}</span></td>
                             <td>{l.price}</td>
-                            <td><span className="glyphicon glyphicon-user" aria-hidden="true"></span>{l.user}</td>
+                            <td><img src={l.image} className="img-circle" alt="Cinque Terre" width="20" height="20"/>{"     " + l.user}</td>
                             <td><Rater interactive={false} rating={l.userRating} /></td>
                             <td>{l.added}</td>
                         </tr>)
@@ -73,7 +119,10 @@ class Content extends Component{
                     }
                     </tbody>
                 </table>
+                <br/><br/>
+                <br/>
             </div>
+
         );
     }
 }
