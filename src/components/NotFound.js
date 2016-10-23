d5584657ac855d2c7ed02f8d40ef2fb3852f4f75
Markus Andresen <markua@stud.ntnu.no>
2016-10-23 21:45:15 +0200

    added pathname to 404 message

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -6,8 +6,11 @@ import '../../public/styles/style.css';
 export default class NotFound extends Component {
     render() {
         return (
-            <div>
+            <div className="not-found">
                 <h1>
+                  {this.props.location.pathname}
+                  <br/>
+                  <br/>
                     404 <small>Not Found :(</small>
                 </h1>
             </div>
