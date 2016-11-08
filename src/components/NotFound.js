e5dcd8c564630204aafaac3841f02100a86da612
Markus Andresen <markua@stud.ntnu.no>
2016-11-08 02:09:01 +0100

    route path removed from render

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,21 +1,16 @@
 /**
  * Component to be displayed if the attempted target route doesn't exist or user don't have access to it.
  */
+  import React, { Component } from 'react';
 
-import React, { Component } from 'react';
+  import '../../public/styles/style.css';
 
-import '../../public/styles/style.css';
+  export default class NotFound extends Component {
 
-export default class NotFound extends Component {
-
-    /**
-     * @returns {XML}
-     */
     render() {
         return (
             <div className="not-found">
                 <h1>
-                  {this.props.location.pathname}
                   <br/>
                   <br/>
                     404 <small>Not Found :(</small>
