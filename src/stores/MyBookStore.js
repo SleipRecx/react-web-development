7a609622eede88ce30b1e3dc35fc9cc3e4b33991
Markus Andresen <markua@stud.ntnu.no>
2016-11-09 16:31:21 +0100

    bookstore logic

--- src/stores/MyBookStore.js
+++ src/stores/MyBookStore.js
@@ -1,5 +1,7 @@
 import { EventEmitter } from "events";
 import loginStore from "./LoginStore"
+import dispatcher from "../dispatcher";
+var queryString = require('query-string');
 
 class MyBookStore extends EventEmitter{
   constructor(){
@@ -19,7 +21,7 @@ class MyBookStore extends EventEmitter{
   }
 
   fetchBooks(id){
-    var url = "http://localhost:9001/api/books/user/" + id
+    var url = "http://localhost:9001/api/user_books/" + id
     fetch(url).then(r => r.json())
     .then(data => {
       this.books = data;
@@ -27,7 +29,49 @@ class MyBookStore extends EventEmitter{
     })
   }
 
+async addNewBook(book_data){
+    var id = undefined
+    await this.getUserData().then(result =>{
+      id = result.id
+    });
+
+    var payload = {
+    "title": book_data.title,
+    "author": book_data.author,
+    "price": book_data.price,
+    "state": book_data.state,
+    "user_id_foreign": id};
+    var data = queryString.stringify(payload)
+      fetch("http://localhost:9001/api/book",
+      {
+          method: "POST",
+          body: data,
+          headers: {"Content-Type": "application/x-www-form-urlencoded"}
+      })
+      .then(r => r.json()).then(data => {
+        this.fetchBooks(id);
+      })
+
+  }
+
+
+  handleActions(action){
+    switch(action.type){
+      case "ADD_BOOK": {
+          this.addNewBook(action.data)
+          break;
+      }
+      default: {
+        break;
+      }
+
+    }
+  }
+
+
 }
 
 const myBookStore = new MyBookStore();
+dispatcher.register(myBookStore.handleActions.bind(myBookStore));
+window.dispatcher = dispatcher;
 export default myBookStore;
