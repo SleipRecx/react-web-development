e703f73d6b420eb1c3a4d77c8ebfff6b36cb65d6
Markus Andresen <markua@stud.ntnu.no>
2016-11-12 22:20:10 +0100

    lorem ispum replaced with faker

--- src/components/MyBooks.js
+++ src/components/MyBooks.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import myBookStore from '../stores/MyBookStore';
-import ResultObject from './ResultObject';
+import ResultObject from './ResultObjectMyBook';
 import '../../public/styles/style.css';
 import label_converter from '../data/label_converter';
 import ReactDOM from 'react-dom';
-import * as LoginActions from '../stores/LoginActions'
+import * as Actions from '../stores/Actions'
 import { default as swal } from 'sweetalert2'
 import "sweetalert2/dist/sweetalert2.min.css"
 
@@ -13,8 +13,21 @@ export default class MyBooks extends Component {
       super(props);
       this.getBooksFromStore = this.getBooksFromStore.bind(this);
       this.getNewBookFromStore = this.getNewBookFromStore.bind(this);
+      this.getBooksFromStoreAfterDelete = this.getBooksFromStoreAfterDelete.bind(this)
       this.state = {data: myBookStore.getAllBooks()};
       this.buttonPressed = this.buttonPressed.bind(this);
+  }
+
+  getBooksFromStoreAfterDelete(){
+    this.setState({
+      data: myBookStore.getAllBooks()
+    });
+    swal({
+      title: 'Delete successful',
+      type: 'success',
+      timer: 1100,
+      showConfirmButton: false
+    }).catch(swal.noop);
   }
 
   getBooksFromStore(){
@@ -39,11 +52,13 @@ export default class MyBooks extends Component {
   componentWillMount(){
     myBookStore.on("data_loaded", this.getBooksFromStore);
     myBookStore.on("new_book", this.getNewBookFromStore);
+    myBookStore.on("delete_book", this.getBooksFromStoreAfterDelete);
   }
 
   componentWillUnmount(){
     myBookStore.removeListener("data_loaded", this.getBooksFromStore);
     myBookStore.removeListener("new_book", this.getNewBookFromStore);
+    myBookStore.removeListener("delete_book", this.getBooksFromStoreAfterDelete);
   }
 
 clearForm(){
@@ -60,7 +75,7 @@ buttonPressed(e) {
     object.state = form.elements.optradio.value
     object.title =  ReactDOM.findDOMNode(this.refs.title).value
     object.author = ReactDOM.findDOMNode(this.refs.author).value
-    LoginActions.addBook(object);
+    Actions.addBook(object);
 
 }
 
@@ -116,30 +131,29 @@ buttonPressed(e) {
               <center><h3>Your Books</h3></center>
 
                   <div className="result-table container">
-                      <div className="row">
+                      <div >
                           <ul className="list-inline row result-object result-object-header color">
                               <li className="col-sm-3">
                                   Title
                               </li>
+                              <li className="col-sm-2">
+                                  Author
+                              </li>
                               <li className="col-sm-2 price">
                                   Condition
                               </li>
-                              <li className="col-sm-1 price">
-                                  Price
-                              </li>
-                              <li className="col-sm-3">
-                                  Seller
-                              </li>
                               <li className="col-sm-2 price">
-                                  Rating
+                                  Price
                               </li>
                               <li className="col-sm-2">
                                   Added
                               </li>
+                              <li className="col-sm-1 price">
+                                Remove
+                              </li>
                           </ul>
                           {this.state.data.map(function(l){ return (
-                              <ResultObject key={l.book_id} title={l.title} state={l.state} price={l.price} user={l.first_name + " " + l.last_name}
-                                            userRating={l.rating} added={l.date_added.split("T")[0]} image={l.image_link} />
+                              <ResultObject key={l.book_id} title={l.title} author={l.author} state={l.state} price={l.price} id={l.book_id} added={l.date_added.split("T")[0]} />
                           )})}
                       </div>
                   </div>
--- src/components/ResultObjectDetails.js
+++ src/components/ResultObjectDetails.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import '../../public/styles/style.css';
 import {Link} from 'react-router';
+var faker = require('faker');
 
 export default class ResultObjectDetails extends Component {
 
@@ -8,26 +9,20 @@ export default class ResultObjectDetails extends Component {
         return (
             <div className="result-object-details">
                 <row>
-                    <div className="col-xs-4">
+                    <div className="col-xs-3">
                         <div className="placeholder-image">
-
+                        <span className="glyphicon glyphicon-book"></span>
                         </div>
                     </div>
                     <div className="col-xs-8">
                         <h3>Title: {this.props.title}</h3>
-                        <p><b>Description:</b> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            Pellentesque a finibus leo, sit amet interdum massa. Vestibulum convallis
-                            sapien nec odio placerat, nec condimentum neque volutpat. Duis porttitor
-                            commodo augue a euismod. Maecenas non placerat sem. Suspendisse sollicitudin
-                            ex ut molestie tempor. Cras in porta metus. Maecenas dapibus dapibus diam a venenatis.
-                            Cras et metus a metus dictum blandit. Aliquam erat volutpat. Vestibulum purus erat, cursus
-                            in justo a, sodales gravida nisl. Fusce gravida urna neque, ut facilisis dolor facilisis eu.</p>
+                        <p><b>Description:</b>{" " +faker.lorem.sentences()}</p>
                         <p><b>Author:</b> {this.props.author}</p>
-                        <p><b>Seller:</b>  <Link to={"/seller/" + this.props.id}>{this.props.user}</Link>
+                        <p className="inline"><b>Seller:</b>  <Link to={"/seller/" + this.props.id}>{this.props.user}</Link>  </p>
                             <form className="pull-right" action="mailto:seller@bookshelf.com">
                                 <button type="submit" className="btn btn-primary" >Contact seller <i className="glyphicon glyphicon-envelope"></i></button>
                             </form>
-                        </p>
+
                     </div>
                     <div className="clearfix"></div>
                 </row>
