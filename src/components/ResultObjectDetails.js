618035cafdf0efb05a779b8203d32e5aa3c09a81
Stian Dysthe <dysthe_stian@hotmail.com>
2016-11-12 17:27:03 +0100

    Added bold to description, seller and author

--- src/components/ResultObjectDetails.js
+++ src/components/ResultObjectDetails.js
@@ -15,15 +15,15 @@ export default class ResultObjectDetails extends Component {
                     </div>
                     <div className="col-xs-8">
                         <h3>Title: {this.props.title}</h3>
-                        <p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        <p><b>Description:</b> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                             Pellentesque a finibus leo, sit amet interdum massa. Vestibulum convallis
                             sapien nec odio placerat, nec condimentum neque volutpat. Duis porttitor
                             commodo augue a euismod. Maecenas non placerat sem. Suspendisse sollicitudin
                             ex ut molestie tempor. Cras in porta metus. Maecenas dapibus dapibus diam a venenatis.
                             Cras et metus a metus dictum blandit. Aliquam erat volutpat. Vestibulum purus erat, cursus
                             in justo a, sodales gravida nisl. Fusce gravida urna neque, ut facilisis dolor facilisis eu.</p>
-                        <p>Author: {this.props.author}</p>
-                        <p>Seller:  <Link to={"/seller/" + this.props.id}>{this.props.user}</Link></p>
+                        <p><b>Author:</b> {this.props.author}</p>
+                        <p><b>Seller:</b>  <Link to={"/seller/" + this.props.id}>{this.props.user}</Link></p>
                     </div>
                     <div className="clearfix"></div>
                 </row>
