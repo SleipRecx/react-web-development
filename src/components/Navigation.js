e8a8197d3804a559593a647f3376a4bacda7aee9
Stian Dysthe <dysthe_stian@hotmail.com>
2016-11-13 21:52:53 +0100

    Removed outdated comment

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,8 +4,6 @@
  * Imports Link from react-router in order to be able to change the component when changing route, instead
  * of reloading the entire page, which you would by using an anchor tag with a href.
  *
- * Imports nav_options in order to keep the data separated from the view, making it easier to keep control of
- * the navigation options and their routes.
  */
 
 import React, { Component} from 'react';
