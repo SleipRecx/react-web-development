3a1752d949908418193fbf5cd7d7f252f4b3e2b2
Stian Dysthe <dysthe_stian@hotmail.com>
2016-11-13 16:36:09 +0100

    Fetch books related to seller from api

--- src/stores/SellerStore.js
+++ src/stores/SellerStore.js
@@ -4,11 +4,16 @@ import dispatcher from "../dispatcher";
 class SellerStore extends EventEmitter{
   constructor(){
     super();
-    this.seller = {}
+    this.seller = {};
+    this.books = [];
   }
 
   getSeller(){
     return this.seller[0]
+  }
+
+  getBooks(){
+    return this.books;
   }
 
 
@@ -18,6 +23,16 @@ class SellerStore extends EventEmitter{
     .then(data => {
       this.seller = data
       this.emit("new_data")
+    })
+    .catch(e => console.log(e))
+  }
+
+  fetchBooks(id){
+    var url = "http://localhost:9001/api/books/user/" + id;
+    fetch(url).then(r => r.json())
+    .then(data => {
+      this.books = data;
+      this.emit("new_books")
     })
     .catch(e => console.log(e))
   }
